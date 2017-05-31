57c74d8f82b5bb2355ed5dbf5c9e5c5d191a3c68
Ernest S <ernest@gmail.com>
2017-05-31 12:12:32 -0700

    Remove hard-coded relation count

--- PTransE/PCRA.py
+++ PTransE/PCRA.py
@@ -25,15 +25,19 @@ for line in f:
     id2relation[int(seg[1])]=seg[0]
     id2relation[int(seg[1])+1345]="~"+seg[0]
     relation_num+=1
+    
+for line in f:
+    seg = line.strip().split()
+    id2relation[int(seg[1])+relation_num]="~"+seg[0]
 f.close()
-f = open("data/train.txt","r")
+
 ok = {}
 a ={}
 
 num=0
 step=0
 
-
+f = open("data/train.txt","r")
 for line in f:
     seg = line.strip().split()
     e1 = seg[0]
